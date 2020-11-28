84fe8aafd790e03a0224668c02651145bd014345
Albin Médoc <stamline203@gmail.com>
2020-11-28 22:58:43 +0100

    Created type decleration, updated package name

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+import type { SvelteComponent } from "svelte";
+
+export interface FormProps {
+    method: string;
+    action: string;
+}
+
+declare class Form extends SvelteComponent {
+    $$prop_def: FormProps;
+}
+
+declare class FormRow extends SvelteComponent { }
+
+export interface InputContainerProps {
+    label: string;
+}
+
+declare class InputContainer extends SvelteComponent {
+    $$prop_def: InputContainerProps;
+}
+
+export { Form, FormRow, InputContainer }
